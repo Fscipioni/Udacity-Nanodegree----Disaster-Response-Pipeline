dcb675a1f74dbf9b335917c02fa76a0c1131daa9
Francesca.Scipioni <fra.scipioni.83@gmail.com>
2022-08-19 22:49:42 +0000

    Removed class StartingVerbExtractor

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -22,25 +22,9 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-    
+   
 def load_data(database_filepath):
     engine = create_engine('sqlite:///DisasterResponse.db')
     df = pd.read_sql_table('DisasterResponse', engine)
@@ -67,37 +51,37 @@ def tokenize(text):
 
 
 def build_model():
-    pipeline = Pipeline([
-        ('features', FeatureUnion([
+    pipeline = Pipeline([('vect', CountVectorizer(tokenizer = tokenize)), 
+                 ('tfidf', TfidfTransformer()), 
+                 ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                ])
 
-            ('text_pipeline', Pipeline([
-                ('vect', CountVectorizer(tokenizer=tokenize)),
-                ('tfidf', TfidfTransformer())
-            ])),
 
-            ('starting_verb', StartingVerbExtractor())
-        ])),
-
-        ('clf', RandomForestClassifier())
-    ])
-
-    parameters = {
-        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
-        'clf__n_estimators': [50, 100, 200],
-        'clf__min_samples_split': [2, 3, 4]
+    print('Searching for best parameters...')
+    parameters = {'vect__ngram_range': ((1, 1), (1, 2))
+        , 'vect__max_df': (0.5, 0.75, 1.0)
+        , 'clf__estimator__n_estimators': [50, 100, 200]
+        , 'clf__estimator__min_samples_split': [2, 3, 4]
     }
-
+    
     cv = GridSearchCV(pipeline, param_grid=parameters)
 
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    y_pred = model.predict(X_test)
+    
+    for i, col in enumerate(category_names):
+       print('Category: {} '.format(col)
+       print(classification_report(Y_test.iloc[:, i].values, Y_pred[:, i]))
+       print('Accuracy {}\n\n'.format(accuracy_score(Y_test.iloc[:, i].values, Y_pred[:, i])))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open (model_filepath, 'wb') as f:
+    pickle.dump(model, f)
 
 
 def main():
